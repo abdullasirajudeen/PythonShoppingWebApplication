2729182dd2cc27a1ebf86ae470413979bd3db9ab
Akhil MS <akhilsree001@gmail.com>
2020-07-12 10:32:19 +0530

    Revert "Merge branch 'master' into wip"
    This reverts commit cd3a5b79a9cc14fce58c946daa21e24fefdb34e0, reversing
    changes made to 28fee1f050c2c973ae3298e4d08aea1df897f7ca.

--- OnlineLocalStore/OnlineLocalStore/env_localstore/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\Main Project\VishnuMigrate\OnlineLocalStore\OnlineLocalStore\env_localstore\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
