d484d459cfedc4af4ea4c33eec7e26069edb33cb
Akhil MS <akhilsree001@gmail.com>
2020-07-12 10:53:10 +0530

    show product name instaed of "objectname"

--- OnlineLocalStore/shop/models.py
+++ OnlineLocalStore/shop/models.py
@@ -16,3 +16,7 @@ class products(models.Model):
 	offerprice = models.IntegerField(blank=True,null=True)
 	created_at = models.DateField(auto_now_add=True)
 	owner = models.ForeignKey(User,default=None,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.pname
+		
